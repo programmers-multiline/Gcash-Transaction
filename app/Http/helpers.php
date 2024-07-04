7276d2f45a249baf74176a6d751a46336acddf94
Jhon Paolo Gaita <jhonpaologaita16@gmail.com>
2024-07-04 11:44:46 +0800

    Update helpers.php

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -2,9 +2,9 @@
 
 function add_public(){
     if(env("APP_ENV") == 'local'){
-        echo env('ASSET_URL', '/');
+         env('ASSET_URL', '/');
     }else{
-        echo env('ASSET_URL', '/public');
+         env('ASSET_URL', '/public');
     }
 }
 
